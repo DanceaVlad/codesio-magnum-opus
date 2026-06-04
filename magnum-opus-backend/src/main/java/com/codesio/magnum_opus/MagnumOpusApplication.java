bbb163aec7d4a7eb221364f79a9d0ff773610f20
Vlad Dancea <danceavlad@proton.me>
2026-06-04 12:52:50 +0200

    task: add angular and springboot scaffolding

---
+++ magnum-opus-backend/src/main/java/com/codesio/magnum_opus/MagnumOpusApplication.java
@@ -0,0 +1,13 @@
+package com.codesio.magnum_opus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagnumOpusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagnumOpusApplication.class, args);
+	}
+
+}
